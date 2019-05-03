2639bfb03580a733b86fd2a622f5833267ebe14a
徳山 来人 <tokuyamaraido@team-lab.com>
2019-05-03 11:31:09 +0900

    190503 [add]phone number validation

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $(function () {
     const MSG_TEXT_MAX = '20文字以内で入力してください';
     const MSG_EMAIL_TYPE = 'EMAILの形式で入力してください';
     const MSG_COMMENT_MAX = '100文字以内で入力してください';
+    const MSG_PHONE_NUMBER_LENGTH = '電話番号は11桁の携帯番号で入力してください';
+    const MSG_PHONE_NUMBER_PATTERN_ERR = '電話番号の入力形式が間違っています';
 
     $('.valid-text').keyup(function () {
         var form_g = $(this).closest('.form-group');
@@ -32,6 +34,39 @@ $(function () {
             form_g.removeClass('has-error').addClass('has-success');
             form_g.find('.help-block').text('');
         }
+    });
+
+    $('.format-number').change(function () {
+        var form_g = $(this).closest('.form-group');
+
+        // 入力値からハイフンを取り除く
+        var format_before = $(this).val();
+        format_before = format_before.replace(/-/g, '');
+
+        // 入力値をすべて半角に直す
+        var format_after = format_before.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) {
+            return String.fromCharCode(s.charCodeAt(0) - 0xFEE0)
+        });
+
+        // 入力された文字数判定
+        if (format_after.length === 11) {
+            $(this).val(format_after.substr(0, 3) + '-' + format_after.substr(3, 4) + '-' + format_after.substr(7, 4));
+            console.log($(this).val());
+            var format_phone_number_pattern = $(this).val().match(/^(070|080|090)-\d{4}-\d{4}$/);
+
+            if (format_phone_number_pattern) {
+                form_g.removeClass('has-error').addClass('has-success');
+                form_g.find('.help-block').text('');
+            } else {
+                form_g.removeClass('has-success').addClass('has-error');
+                form_g.find('.help-block').text(MSG_PHONE_NUMBER_PATTERN_ERR);
+            }
+        } else {
+            $(this).val(format_after);
+            form_g.removeClass('has-success').addClass('has-error');
+            form_g.find('.help-block').text(MSG_PHONE_NUMBER_LENGTH);
+        }
+
     });
 
     $('.valid-textarea').keyup(function () {
